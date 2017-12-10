ce1359801214b94877f7724da1d735c368e862b9
erdiolmezogullari <erdi.olmez@gmail.com>
2017-12-10 16:16:13 +0300

    Initial commit

--- KMeans.java
+++ KMeans.java
@@ -1,215 +1,252 @@
-///////////////////////////////////////////////////////////////////////////////
-// Title:            hw3
-// Files:            HW3.java, KMeansResult.java, KMeans.java
-// Semester:         CS 540 Spring 2016
-//
-// Author:           Jale Dinler
-// Email:            dinler@wisc.edu
-// CS Login:         jale
-// Lecturer's Name:  Collin Engstrom
-
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.HashMap;
 
 /**
- * A k-means clustering algorithm implementation with Euclidean distance.
- * @author Jale Dinler
- *
+ * Created by eolmezogullari on 11/30/2015.
  */
-public class KMeans {
-	/**
-	 * @param centroids 2D array to store the centroids
-	 * @param instances 2D array to store the instances
-	 * @param threshold for stopping the iterations of the algorithm
-	 * @return a KMeansResult object
-	 */
-	public KMeansResult cluster(double[][] centroids, double[][] instances, double threshold) {
-		//stores a KMeansReasult object
-		KMeansResult result = new KMeansResult();
-		//stores a distortion list that keeps the calculated distortions for each iteration
-		LinkedList<Double> distortions = new LinkedList<Double>();
-		//create the cluster assignment array
-		result.clusterAssignment = new int[instances.length];
-		//stores the change in distortion between successive iterations
-		double changeInDistortion = 0;
+public class KMeans
+{
+    private static final String FORMAT_OF_OUTPUT = "%s,%d";
+    private static final String HEADER_OF_OUTPUT = "Country,Group";
 
-		// Loop until done
-		do {
-			//initializing instances
-			for(int i=0; i < instances.length; i++)
-			{
-				int centroidIndex = -1;
-				double minDist = Double.MAX_VALUE;
-				//iterate through centroids
-				for(int k = 0; k < centroids.length; k++)
-				{
-					double dist = 0;
-					//iterate through instances and calculate the distance of instance i
-					//to the centroid k
-					for(int j = 0; j < instances[i].length; j++)
-					{
-						dist+=Math.pow(instances[i][j]-centroids[k][j], 2);
-					}
-					dist = Math.sqrt(dist);
+    private static final double     MIN_THRESHOLD = 0.001;
+    private final static double[][] table         = {{10326.00, 90.00, 23.60, 75.40}, {39650.00, 99.00, 4.08, 79.40}, {830.00, 38.70, 95.90, 42.10}, {43163.00, 99.00, 4.57, 81.20}, {5300.00, 90.90, 23.00, 73.00}, {13308.00, 97.20, 13.40, 75.30}, {34105.00, 99.00, 5.01, 79.40}, {10600.00, 82.40, 44.80, 49.30}, {1000.00, 68.00, 92.70, 42.40}, {5249.00, 85.00, 42.30, 52.90}, {4200.00, 100.00, 17.36, 71.00}, {3320.00, 49.90, 67.50, 65.50}, {2972.00, 61.00, 55.00, 64.70}, {12888.00, 88.70, 27.50, 71.80}, {34735.00, 99.00, 3.20, 80.90}, {19730.00, 99.60, 8.50, 73.00}, {36983.00, 96.00, 5.34, 79.50}, {26760.00, 98.50, 5.94, 80.00}, {34099.00, 99.00, 3.20, 82.60}};
+    private final static String[] countries = {"Brazil", "Germany", "Mozambique", "Australia", "China", "Argentina", "United Kingdom", "South Africa", "Zambia", "Namibia", "Georgia", "Pakistan", "India", "Turkey", "Sweden", "Lithuania", "Greece", "Italy", "Japan"};
+    private volatile static double[][] centroids = {{10326.00, 90.00, 23.60, 75.40}, {39650.00, 99.00, 4.08, 79.40}, {830.00, 38.70, 95.90, 42.10}};
 
-					//if the distance between instance i and centroid k is smaller than 
-					//minDist, change minDist to dist and assign instance i to centroid k
-					if(dist < minDist)
-					{
-						minDist=dist;
-						centroidIndex = k;
-					}
-					result.clusterAssignment[i] = centroidIndex;
-				}
-			}
-			//done initializing
+    public static void main(String[] args)
+    {
+        System.out.println("==========Before Normalization==========");
+        run();
 
-			//checking if there is any orphaned centroid
-			boolean isOrphan = false;
-			while(!isOrphan)
-			{
-				//initialize the index of orphaned centroid to -1
-				int orphanIndex = -1;
+        System.out.println("==========After Normalization==========");
+        normalize(table);
+        run();
 
-				//stores a counter to be able to say if there is an orphaned centroid
-				int count = 0;
+    }
 
-				//iterate through centroids
-				for(int k = 0; k < centroids.length; k++)
-				{
-					//iterate through cluster assignments
-					for(int i = 0; i < result.clusterAssignment.length; i++)
-					{
-						//if an instance i is not assigned to the centroid k, increase the count by 1
-						if(result.clusterAssignment[i] != k)
-						{
-							count++;
-						}
-					}
-					//when we are done iterating through cluster assignments and if there is 
-					//no instance that is assigned to the centroid k, then k is orphaned 
-					if(count == result.clusterAssignment.length)
-					{
-						orphanIndex = k;
-						break;
-					}
-				}
-				//if there is an orphaned index, assign the proper instance to that centroid
-				if(orphanIndex > -1)
-				{
-					//initialize the maximum distance to zero
-					double maxDist = 0;
+    public static void run()
+    {
+        final int K = centroids.length;
 
-					//initialize the index of the instance with the max distance to its 
-					//assigned centroid to -1
-					int index = -1;
+        double[][]                           initialCentroids;
+        HashMap<Integer, ArrayList<Integer>> map;
+        do
+        {
+            initialCentroids = centroids.clone();
+            map = kmeans(centroids, table);
+        } while (!isRepeat(K, initialCentroids, centroids, MIN_THRESHOLD));
 
-					//iterate through instances
-					for(int i = 0; i < instances.length; i++)
-					{ 
-						double dist = 0;
-						//find the distance of the instance i to its assigned centroid
-						for(int j = 0; j < instances[0].length; j++)
-						{
-							dist += Math.pow(instances[i][j]-centroids[result.clusterAssignment[i]][j], 2);
-						}
-						dist = Math.sqrt(dist);
+        StringBuilder sb = new StringBuilder(HEADER_OF_OUTPUT);
+        sb.append("\n");
+        sb.append("==================\n");
+        for (int k = 0; k < K; k++)
+        {
+            ArrayList<Integer> indexes = map.get(k);
+            for (int i : indexes)
+            {
+                String country = countries[i];
+                sb.append(String.format(FORMAT_OF_OUTPUT, country, k));
+                sb.append("\n");
+            }
+        }
 
-						//if that distance is greater than the maxDist, change maxDist to that distance
-						if(dist > maxDist)
-						{
-							maxDist = dist;
-							index = i;
-						}
-					}
-					//assign the instance with maximum distance to the orphaned centroid
-					result.clusterAssignment[index] = orphanIndex;
-				}//done with assigning
+        System.out.println(map.toString());
+        System.out.println(sb.toString());
 
-				//do nothing if there is no orphaned centroid
-				else
-					isOrphan = true;
-			}
-			//done with checking
 
-			//reallocating instances
+    }
 
-			//iterate through centroids
-			for(int k = 0; k < centroids.length; k++)
-			{
-				//initialize the total number of instances that are assigned to the centroid k
-				int total = 0;
 
-				//store the sum of each coordinate of the instances that are assigned to the centroid k
-				double[] temp = new double[centroids[k].length];
+    public static boolean isRepeat(int K, double[][] pre, double[][] last, double minThreshold)
+    {
+        for (int k = 0; k < K; k++)
+        {
+            if (diff(pre[k], last[k]) < minThreshold)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 
-				//initialize the sum of coordinates to zero
-				for(int j = 0; j < temp.length; j++)
-				{
-					temp[j]=0;
-				}
+    public static double diff(double[] lhs, double[] rhs)
+    {
+        return distance(lhs, rhs);
+    }
 
-				//iterate throgh instances
-				for(int i = 0; i < result.clusterAssignment.length; i++)
-				{
-					//if the instance i is assigned to the centroid k, increase the number of 
-					//instances that are assigned to the centroid k by 1 and add the each
-					//coordinate j to temp[j] 
-					if(result.clusterAssignment[i] == k)
-					{          
-						for(int j = 0; j < centroids[k].length; j++)
-						{
-							temp[j]+=instances[i][j];
-						}
-						total++;
-					}
-				}
-				//find the new centroids by dividing temp[j] by the total number of instances
-				//that are assigned to the centroid k
-				for(int j = 0; j < centroids[k].length; j++)
-				{	
-					centroids[k][j]=temp[j]/total;
-				}
-			}			
-			//done with reallocating
+    public static HashMap<Integer, ArrayList<Integer>> kmeans(double[][] centroids, double[][] table)
+    {
+        final int K = centroids.length;
+        final int N = table.length;
+        final int C = table[0].length;
 
-			//calculating distortion for the current iteration and adding it to the distortion list
-			double distortion = 0;
-			//iterate through instances
-			for(int i = 0; i < instances.length; i++)
-			{
-				//calculate the distortion of each instance and sum them all
-				for(int j = 0; j < instances[0].length; j++)
-				{
-					distortion+=Math.pow(instances[i][j]-centroids[result.clusterAssignment[i]][j],2);
-				}
-			}
-			//add this iterations distortions to the distortion list
-			distortions.add(distortion);
+        final HashMap<Integer, ArrayList<Integer>> map = new HashMap<Integer, ArrayList<Integer>>();
+        for (int k = 0; k < K; k++)
+        {
+            map.put(k, new ArrayList<Integer>());
+        }
 
-			//calculating change in distortion between successive iterations if we iterated more than once
-			//if we iterated once, simply taking that iteration's distortion
-			if(distortions.size() == 1)
-			{
-				changeInDistortion = distortions.get(0);
-			}
+        final double[][] distance = new double[N][K];
+        for (int k = 0; k < K; k++)
+        {
+            double[] centroid = centroids[k];
+            for (int i = 0; i < N; i++)
+            {
+                double[] row = table[i];
+                distance[i][k] = distance(centroid, row);
+            }
+        }
 
-			else if(distortions.size() > 1)
-			{
-				changeInDistortion = Math.abs((distortions.get(distortions.size()-1)- distortions.get(distortions.size() - 2))/(distortions.get(distortions.size() - 2)));
-			}
+        for (int i = 0; i < N; i++)
+        {
+            int w = argMin(distance[i]);
+            map.get(w).add(i);
+        }
 
-		}while(changeInDistortion >= threshold);// Iterations completed
+        final double[][] newCentroids = new double[K][C];
+        for (int k = 0; k < K; k++)
+        {
+            ArrayList<Integer> indexes = map.get(k);
+            double[] newCentroid = findNewCentroid(indexes, table);
+            newCentroids[k] = newCentroid;
+        }
+        centroids = newCentroids;
 
-		//assigning the results to KMeansResult object
-		result.centroids=centroids;
-		result.distortionIterations = new double[distortions.size()];
-		for(int i = 0; i < distortions.size(); i++)
-		{
-			result.distortionIterations[i] = distortions.get(i);
-		}//done with assigning
+        return map;
+    }
 
-		return result;
-	}
+    public static double[] findNewCentroid(ArrayList<Integer> indexes, double[][] table)
+    {
+        int      C           = table[0].length;
+        double[] newCentroid = new double[C];
+        for (int c = 0; c < C; c++)
+        {
+            int size = indexes.size();
+            if (size == 0)
+            {
+                newCentroid[c] = 0.0;
+            } else
+            {
+                double tmp = 0.0;
+                for (int i : indexes)
+                {
+                    double[] row = table[i];
+                    tmp += row[c];
+                }
+                newCentroid[c] = tmp / (double) indexes.size();
+            }
+        }
+        return newCentroid;
+    }
+
+    public static double sum(double[] data)
+    {
+        double tmp = 0.0;
+        for (int i = 0; i < data.length; i++)
+        {
+            tmp += data[i];
+        }
+
+        return tmp;
+    }
+
+    public static double average(double[] data)
+    {
+        if (data.length == 0)
+            return 0;
+
+        double s = sum(data);
+        return s / data.length;
+    }
+
+    public static double distance(double[] lhs, double[] rhs)
+    {
+        double tmp = 0.0;
+        for (int i = 0; i < lhs.length; i++)
+        {
+            tmp += Math.pow(lhs[i] - rhs[i], 2);
+        }
+        return Math.sqrt(tmp);
+    }
+
+    public static double min(double[] data)
+    {
+        double m = Double.MAX_VALUE;
+
+        for (int i = 0; i < data.length; i++)
+        {
+            if (data[i] < m)
+                m = data[i];
+        }
+        return m;
+    }
+
+    public static int argMin(double[] data)
+    {
+        double m     = Double.MAX_VALUE;
+        int    index = 0;
+        for (int i = 0; i < data.length; i++)
+        {
+            if (data[i] < m)
+            {
+                m = data[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+
+
+    public static double findMin(int c, double[][] table)
+    {
+        int    N   = table.length;
+        double min = Double.MAX_VALUE;
+        for (int i = 0; i < N; i++)
+        {
+            if (table[i][c] < min)
+            {
+                min = table[i][c];
+            }
+        }
+        return min;
+    }
+
+
+    public static double findMax(int c, double[][] table)
+    {
+        int    N   = table.length;
+        double max = Double.MIN_VALUE;
+        for (int i = 0; i < N; i++)
+        {
+            if (table[i][c] > max)
+            {
+                max = table[i][c];
+            }
+        }
+        return max;
+    }
+
+
+    public static void normalize(double[][] table)
+    {
+        int C = table[0].length;
+        for (int c = 0; c < C; c++)
+        {
+            normalize(c, table);
+        }
+    }
+
+    public static void normalize(int c, double[][] table)
+    {
+        int    N   = table.length;
+        double max = findMax(c, table);
+        double min = findMin(c, table);
+
+        for (int i = 0; i < N; i++)
+        {
+            double newValue = (table[i][c] - min) / (max - min);
+            table[i][c] = newValue;
+        }
+    }
 }
